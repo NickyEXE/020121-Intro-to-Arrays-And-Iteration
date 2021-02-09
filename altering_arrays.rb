e7e9d9f3509c18c2d864cda9dafd225584a53947
NickyEXE <dovern42@gmail.com>
2021-02-09 18:56:39 -0500

    ends altering_arrays portion of study group

--- altering_arrays.rb
+++ altering_arrays.rb
@@ -3,14 +3,29 @@ require 'pry'
 
 # change a specific element
 string_array = ["a", "b", "c"]
-binding.pry
 # How can I change the string_array to make it look like this:
 # ["a", "bee", "c"]
+string_array[1] = "bee"
 
 # add to the end of an array
+  # shovel method
+  string_array << "d"
+
+  # push method
+  string_array.push("e")
+
+  # don't do this
+  string_array[5] = "f"
 
 # add to the beginning of an array
+  string_array.unshift(0)
 
 # remove from end of an array
+  string_array.pop
+  # returns the last item and removes it from the array
 
 # remove from front of an array
+  string_array.shift
+  # returns the last item and removes it from the array
+
+binding.pry
